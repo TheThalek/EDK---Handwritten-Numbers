5088708556b5fc53ffb2a36d565c9b19d482f2f4
Sivaranjith Sivarasa <ranjith1210000@gmail.com>
2022-05-02 09:54:49 +0200

    Nearest neighbor

--- Task_1a.m
+++ Task_1a.m
@@ -9,14 +9,9 @@ data.test_labels = rawdata.testlab;
 data.test_data = rawdata.testv;
 
 %% Variables
-chunk_training_size = 6000;
-chunk_test_size = 1000;
-
-training_chunks = (size(data.train_labels) / chunk_training_size);
-N_training_chunks = training_chunks(1);
-
-test_chunks = (size(data.test_labels) / chunk_test_size);
-N_test_chunks = test_chunks(1);
+number_of_chunks = 60;
+chunk_training_size = rawdata.num_train / number_of_chunks;
+chunk_test_size = rawdata.num_test / number_of_chunks;
 
 num_of_classes = 10;
 
@@ -26,7 +21,7 @@ confusion_matrix = zeros(num_of_classes, num_of_classes);
 classification_table = zeros(size(data.test_data, 1), 2);
 
 
-for chunk_index = 1:N_training_chunks %Iteratin through all chunks
+for chunk_index = 1:1%number_of_chunks %Iteratin through all chunks
     %% Making a new data set for the i-chunk:
     %Finding data for training-set
     chunk_training_start = (chunk_index - 1)*chunk_training_size + 1; %Figuring out where the start of the chunk is
@@ -52,48 +47,14 @@ for chunk_index = 1:N_training_chunks %Iteratin through all chunks
     %% Finding the chunk confusion matrix and chunk classificaion table
     chunk_confusion_matrix = zeros(num_of_classes, num_of_classes);
     chunk_classification_table = zeros(size(chunk_data.test_data, 1), 2);
-    distance_matrix = dist(chunk_data.test_data, chunk_data.train_data');
-    
-    k = 1;
-    [min_values, min_indices] = mink(distance_matrix', k, 1);
-    
-    size_chunk_test_data = size(chunk_data.test_data, 1);
+    distance_matrix = dist(chunk_data.train_data, chunk_data.test_data');
 
-    for j = 1:size_chunk_test_data
-        min_index = min_indices(:, j);
-        classified_labels = chunk_data.train_labels(min_index);
-        disp(classified_labels);
-        
-        unique_classes = unique(classified_labels);
-        disp(unique_classes);
-        bins = histcounts(classified_labels, unique_classes);
-        modes = unique_classes(bins == max(bins));
-        
-        min_distance = -1;
-        classified_label = -1;
-        
-        for k = 1:size(modes, 2)
-            class = modes(1, k);
-            distance = 0;
-            
-            for index = 1:size(min_index, 2)
-                if class == tempate_labels(index)
-                    distance = distance + min_values(index);
-                end
-            end
-            
-            if distance < min_distance || min_distance == -1
-                min_distance = distance;
-                
-                classified_label = class + 1;
-            end
-    
-        end
-        
-        correct_label = chunk_data.test_labels(j) + 1;
-        chunk_confusion_matrix(correct_label, classified_label) = confusion_matrix(correct_label, classified_label) + 1;
-       
-        chunk_classification_table(j, :) = [correct_label - 1 classified_label -1];
+    %% Find the classification for each 
+    for test_pred_index = 1:size(distance_matrix, 2)
+         distance_vector = distance_matrix(:, test_pred_index);
+         [min_value, min_index] = min(distance_vector);
+         predicted_label = chunk_data.train_labels(min_index)
+         actual_label = chunk_data.test_labels(test_pred_index)
     end
     
     %% Adding the chunk matrices to get the final matrices
